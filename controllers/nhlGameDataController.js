49c6968b6c7218e0258306864cf5230de33c000e
Dylan Smith <dylantsmith@gmail.com>
2013-10-14 19:06:21 -0500

    redirect to our main view for /

--- controllers/nhlGameDataController.js
+++ controllers/nhlGameDataController.js
@@ -1,4 +1,8 @@
 module.exports = function(app) {
+    app.get("/", function(req, res) {
+        // redirect to nhl-game-data
+        res.redirect("/nhl-game-data");
+    });
     app.get("/nhl-game-data*", function(req, res) {
         res.render("nhl-game-data");
     });
