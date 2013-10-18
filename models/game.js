7af559ad9852956d813d309400a9a145e8a9f6d2
Dylan Smith <dylantsmith@gmail.com>
2013-10-17 21:03:24 -0500

    Additional game attributes
    Add additional game attributes to include the game time UTC, who won
    the game, and the time the game will be available if it was blacked
    out. Add moment for help in determining times.

--- models/game.js
+++ models/game.js
@@ -1,3 +1,11 @@
+var moment = require("moment");
+
+/**
+ * Removes the scores from the teams string (if it exists)
+ *
+ * @param  {String} teams The teams with optional score
+ * @return {String}       The teams without a game score
+ */
 var teamsWithoutScores = function(teams) {
     var removeDigitsRegex;
 
@@ -6,6 +14,12 @@ var teamsWithoutScores = function(teams) {
     return teams.match(removeDigitsRegex).join(" ");
 };
 
+/**
+ * Returns true if the game is over
+ *
+ * @param  {String}  teams The teams with an optional score
+ * @return {Boolean}       true iff the game is over (has a score)
+ */
 var isGameOver = function(teams) {
     if (teams.indexOf("-") > 0) {
         return true;
@@ -14,6 +28,38 @@ var isGameOver = function(teams) {
     }
 };
 
+/**
+ * Returns the team who won the game (or "tie" if tied)
+ *
+ * @param  {String} teams The teams string with score
+ * @return {String}       The team who won or "tie" if tied
+ */
+var whoWon = function(teams) {
+    var teamsWithScores, team1, team1Score, team2, team2Score;
+
+    // first split the teams into individual team + score
+    teamsWithScores = teams.split(" - ");
+    // determine each team and score
+    team1 = teamsWithScores[0].slice(0, teamsWithScores[0].lastIndexOf(" ")).trim();
+    team1Score = teamsWithScores[0].slice(teamsWithScores[0].lastIndexOf(" ")).trim();
+    team2 = teamsWithScores[1].slice(0, teamsWithScores[1].lastIndexOf(" ")).trim();
+    team2Score = teamsWithScores[1].slice(teamsWithScores[1].lastIndexOf(" ")).trim();
+
+    if (team1Score > team2Score) {
+        return team1;
+    } else if (team2Score > team1Score) {
+        return team2;
+    } else {
+        return "tie";
+    }
+};
+
+/**
+ * Returns true if the game is blacked out
+ *
+ * @param  {String}  networks The networks that aired the game (or will air)
+ * @return {Boolean}          true iff the game is blacked out
+ */
 var isGameBlackedOut = function(networks) {
     if ((networks.indexOf("NBC") !== -1) ||
         (networks.indexOf("NHLN-US") !== -1)) {
@@ -23,15 +69,53 @@ var isGameBlackedOut = function(networks) {
     }
 };
 
+/**
+ * Returns the UTC time when this game will be available to watch (because of
+ * black out)
+ *
+ * @param  {Number} gameTimeUTC The game time UTC
+ * @return {Number}             The time in UTC when the game will be available
+ */
+var blackOutAvailableGameTimeUTC = function(gameTimeUTC) {
+    return moment(gameTimeUTC).add("hours", 52).valueOf();
+};
+
+/**
+ * Creates a Game, which has a date and time, teams, and a based on the networks,
+ * if it's blacked out
+ *
+ * @constructor
+ *
+ * @param {String} date     The date the game was aired (or will air)
+ * @param {String} time     The time the game will air
+ * @param {String} teams    The teams that played, along with a score (if game
+ *                          is complete)
+ * @param {String} location The location of the game
+ * @param {String} networks The networks that aired the game (or will air)
+ */
 function Game(date, time, teams, location, networks) {
+    // store the passed in values
     this.date = date;
     this.time = time;
     this.teams = teams;
-    this.teamsWithoutScores = teamsWithoutScores(teams);
     this.location = location;
     this.networks = networks;
+
+    // calculate the UTC game time
+    this.gameTimeUTC = moment(this.date + " " + this.time).valueOf();
+
+    // pull the scores out, figure out if the game is over, and who won
+    this.teamsWithoutScores = teamsWithoutScores(this.teams);
     this.isGameOver = isGameOver(this.teamsWithoutScores);
+    if (this.isGameOver) {
+        this.whoWon = whoWon(this.teams);
+    }
+
+    // populate the blacked out attributes
     this.isBlackedOut = isGameBlackedOut(this.networks);
+    if (this.isBlackedOut) {
+        this.availableGameTimeUTC = blackOutAvailableGameTimeUTC(this.gameTimeUTC);
+    }
 }
 
 module.exports = Game;
