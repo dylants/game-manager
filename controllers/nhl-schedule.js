ccd64b0e4e008dac69d8d11680bf459014b362a8
Dylan Smith <dylantsmith@gmail.com>
2013-10-18 16:56:27 -0500

    Refactor to use async
    So we can get access to the requested team, refactor the import
    schedule API to use async.

--- controllers/nhl-schedule.js
+++ controllers/nhl-schedule.js
@@ -1,5 +1,6 @@
 var csv = require("csv"),
     request = require("request"),
+    async = require("async"),
     NHLGameBuilder = require("../model-builders/nhl-game-builder"),
     mongoose = require("mongoose"),
     Team = mongoose.model("Team");
@@ -40,48 +41,86 @@ module.exports = function(app) {
             nhlGames = [];
             nhlGameBuilder = new NHLGameBuilder();
 
-            request.get(getScheduleUrl(teamName), function(error, response, body) {
-                csv().from.string(body, {
-                    delimiter: ",",
-                    escape: '"'
-                })
-                // when a record is found in the CSV file (a row)
-                .on("record", function(row, index) {
-                    var nhlGame;
-
-                    // skip the header row
-                    if (index === 0) {
-                        return;
-                    }
-
-                    // date, time, teams, location, networks
-                    nhlGame = nhlGameBuilder.buildNHLGame(
-                        row[0].trim(),
-                        row[1].trim(),
-                        row[3].trim(),
-                        row[4].trim(),
-                        row[5].trim());
-                    // add the game to our list
-                    nhlGames.push(nhlGame);
-                })
-                // when the end of the CSV document is reached
-                .on("end", function() {
-                    // save the games to the team
+            async.waterfall([
+                // Get the schedule information
+                function(callback) {
+                    request.get(getScheduleUrl(teamName),
+                        function(error, response, body) {
+                            callback(null, body);
+                        });
+                },
+                // Get the NHL team we're trying to load the schedule for
+                function(csvString, callback) {
                     Team.findOne({
                         shortName: teamName
                     }, function(err, team) {
-                        team.schedule = nhlGames;
-                        team.save(function() {
-                            res.send("done");
-                        });
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback(null, csvString, team);
+                        }
                     });
-                })
-                // if any errors occur
-                .on("error", function(error) {
-                    console.log(error.message);
-                    res.send(500, error.message);
-                });
+                },
+                // Parse the schedule into individual games
+                function(csvString, nhlTeam, callback) {
+                    var nhlGames = [];
+
+                    csv().from.string(csvString, {
+                        delimiter: ",",
+                        escape: '"'
+                    })
+                    // when a record is found in the CSV file (a row)
+                    .on("record", function(row, index) {
+                        var nhlGame;
+
+                        // skip the header row
+                        if (index === 0) {
+                            return;
+                        }
+
+                        // date, time, teams, location, networks
+                        nhlGame = nhlGameBuilder.buildNHLGame(
+                            row[0].trim(),
+                            row[1].trim(),
+                            row[3].trim(),
+                            row[4].trim(),
+                            row[5].trim());
+                        // add the game to our list
+                        nhlGames.push(nhlGame);
+                    })
+                    // when the end of the CSV document is reached
+                    .on("end", function() {
+                        // call the next function with the games
+                        callback(null, nhlGames, nhlTeam);
+                    })
+                    // if any errors occur
+                    .on("error", function(error) {
+                        // call the callback with the error message
+                        callback(error.message);
+                    });
+                },
+                // Save the schedule to the team
+                function(nhlGames, nhlTeam, callback) {
+                    // save the games to the team
+                    nhlTeam.schedule = nhlGames;
+                    nhlTeam.save(function(err) {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback(null);
+                        }
+                    });
+                }
+            ], function(err, result) {
+                if (err) {
+                    console.log(err);
+                    res.send(500, err);
+                    return;
+                }
+
+                res.send("done!");
             });
+
         });
     });
 };
