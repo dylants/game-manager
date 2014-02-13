5b86bed9fda134cb94d1915f088a3b3dbc67739b
Dylan Smith <dylantsmith@gmail.com>
2014-02-12 21:25:09 -0600

    Refactor and pull out logic to functions
    Update the APIs for user UI data to pull out the logic from the APIs to
    make the code a bit more readable.

--- controllers/user-ui-data.js
+++ controllers/user-ui-data.js
@@ -27,61 +27,16 @@ module.exports = function(app) {
                         team = teams[count];
                         count++;
 
-                        // find the matching team object, populating it's schedule of games
-                        Team.findOne({
-                            sport: team.sport,
-                            name: team.team
-                        }).populate("schedule").exec(function(err, team) {
-                            var teamGames, currentTime, teamGamesCounter, teamGame,
-                                userGamesWatched, i;
-
+                        addGamesForTeam(team, user, function(err, gamesForTeam) {
                             if (err) {
                                 whilstCallback(err);
-                                return;
+                            } else {
+                                games = games.concat(gamesForTeam);
+                                whilstCallback();
                             }
-
-                            // get the games for the team
-                            teamGames = team.schedule.toObject();
-
-                            // current time is used to calculate the game state
-                            currentTime = (new Date()).valueOf();
-
-                            // loop over these games to optionally add watched information
-                            for (teamGamesCounter = 0; teamGamesCounter < teamGames.length; teamGamesCounter++) {
-                                teamGame = teamGames[teamGamesCounter].toObject();
-
-                                // if the game has been viewed, update it
-                                userGamesWatched = getGamesWatched(user, team.sport);
-                                for (i = 0; i < userGamesWatched.length; i++) {
-                                    if (userGamesWatched[i].game.toJSON() == teamGame._id.toJSON()) {
-                                        teamGame.notes = userGamesWatched[i].notes;
-                                        teamGame.completed = userGamesWatched[i].completed;
-                                        break;
-                                    }
-                                }
-
-                                // set the game state based off if viewed or current time
-                                if (teamGame.completed) {
-                                    teamGame.gameState = "archived";
-                                } else {
-                                    // if we haven't, is it available to watch?
-                                    if (currentTime > teamGame.availableGameTimeUTC) {
-                                        teamGame.gameState = "available";
-                                    } else {
-                                        teamGame.gameState = "future";
-                                    }
-                                }
-
-                                // add the game to our user's game
-                                games.push(teamGame);
-                            }
-
-                            whilstCallback();
                         });
                     },
                     function(err) {
-                        var availableGames, archivedGames, futureGames, i, game;
-
                         if (err) {
                             console.error(err);
                             res.send(500, {
@@ -90,49 +45,9 @@ module.exports = function(app) {
                             return;
                         }
 
-                        // now that we've got all the games, sort them
-                        games.sort(function(gameA, gameB) {
-                            // sort them by the game time UTC
-                            if (gameA.gameTimeUTC > gameB.gameTimeUTC) {
-                                return 1;
-                            } else if (gameB.gameTimeUTC > gameA.gameTimeUTC) {
-                                return -1;
-                            } else {
-                                return 0;
-                            }
-                        });
-
-                        // with the games sorted, place them in the correct collection
-                        availableGames = [];
-                        archivedGames = [];
-                        futureGames = [];
-                        for (i = 0; i < games.length; i++) {
-                            game = games[i];
-                            switch (game.gameState) {
-                                case "available":
-                                    availableGames.push(game);
-                                    break;
-                                case "archived":
-                                    archivedGames.push(game);
-                                    break;
-                                case "future":
-                                    futureGames.push(game);
-                                    break;
-                                default:
-                                    console.error("unable to place game: " + game);
-                                    break;
-                            }
-                        }
-
-                        // archived games should be last one first, so
-                        // that the user sees the latest archived.
-                        archivedGames.reverse();
-
-                        // send the set of games back
-                        res.send({
-                            availableGames: availableGames,
-                            archivedGames: archivedGames,
-                            futureGames: futureGames
+                        organizeGames(games, function(games) {
+                            // send the set of games back
+                            res.send(games);
                         });
                     }
                 );
@@ -146,54 +61,8 @@ module.exports = function(app) {
             // pull the watched game from the request body
             watchedGame = req.body.game;
 
-            // update the games watched with this watched game information
-            User.findById(req.params.id, function(err, user) {
-                var i, gamesWatched, existingGameWatched, foundMatch;
-
-                if (err) {
-                    console.error(err);
-                    res.send(500, {
-                        "error": err
-                    });
-                    return;
-                }
-
-                for (i = 0; i < user.sportsWatched.length; i++) {
-                    if (watchedGame.sport === user.sportsWatched[i].sport) {
-                        gamesWatched = user.sportsWatched[i].games;
-                        break;
-                    }
-                }
-
-                // if no games watched yet for this sport, create one
-                if (!gamesWatched) {
-                    gamesWatched = [];
-                    user.sportsWatched.push(gamesWatched);
-                }
-
-                // loop through the games watched looking for a matching game
-                for (i = 0; i < gamesWatched.length; i++) {
-                    if (watchedGame._id === gamesWatched[i].game.toJSON()) {
-                        foundMatch = true;
-                        break;
-                    }
-                }
-
-                if (foundMatch) {
-                    // if one already exists, update the existing game
-                    gamesWatched[i].notes = watchedGame.notes;
-                    gamesWatched[i].completed = watchedGame.completed;
-                } else {
-                    // else create a new one
-                    gamesWatched.push({
-                        game: watchedGame._id,
-                        notes: watchedGame.notes,
-                        completed: watchedGame.completed
-                    });
-                }
-
-                // save the updated game
-                user.save(function(err, user) {
+            if (watchedGame) {
+                updateWatchedGameForUser(req.params.id, watchedGame, function(err) {
                     if (err) {
                         console.error(err);
                         res.send(500, {
@@ -204,12 +73,73 @@ module.exports = function(app) {
 
                     res.send(200);
                 });
-            });
+            } else {
+                res.send(400, {
+                    "error": "Unable to determine operation from request body"
+                });
+                return;
+            }
         });
     });
 };
 
-var getGamesWatched = function(user, sport) {
+function addGamesForTeam(team, user, callback) {
+    // find the matching team object, populating it's schedule of games
+    Team.findOne({
+        sport: team.sport,
+        name: team.team
+    }).populate("schedule").exec(function(err, team) {
+        var games, teamGames, currentTime, teamGamesCounter, teamGame,
+            userGamesWatched, i;
+
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        games = [];
+
+        // get the games for the team
+        teamGames = team.schedule.toObject();
+
+        // current time is used to calculate the game state
+        currentTime = (new Date()).valueOf();
+
+        // loop over these games to optionally add watched information
+        for (teamGamesCounter = 0; teamGamesCounter < teamGames.length; teamGamesCounter++) {
+            teamGame = teamGames[teamGamesCounter].toObject();
+
+            // if the game has been viewed, update it
+            userGamesWatched = getGamesWatched(user, team.sport);
+            for (i = 0; i < userGamesWatched.length; i++) {
+                if (userGamesWatched[i].game.toJSON() == teamGame._id.toJSON()) {
+                    teamGame.notes = userGamesWatched[i].notes;
+                    teamGame.completed = userGamesWatched[i].completed;
+                    break;
+                }
+            }
+
+            // set the game state based off if viewed or current time
+            if (teamGame.completed) {
+                teamGame.gameState = "archived";
+            } else {
+                // if we haven't, is it available to watch?
+                if (currentTime > teamGame.availableGameTimeUTC) {
+                    teamGame.gameState = "available";
+                } else {
+                    teamGame.gameState = "future";
+                }
+            }
+
+            // add the game to our user's game
+            games.push(teamGame);
+        }
+
+        callback(null, games);
+    });
+}
+
+function getGamesWatched(user, sport) {
     var gamesWatched, sportsWatched, i;
 
     // get the sports watched for this user
@@ -227,4 +157,104 @@ var getGamesWatched = function(user, sport) {
     }
 
     return gamesWatched;
-};
+}
+
+function organizeGames(games, callback) {
+    var availableGames, archivedGames, futureGames, i, game;
+
+    // now that we've got all the games, sort them
+    games.sort(function(gameA, gameB) {
+        // sort them by the game time UTC
+        if (gameA.gameTimeUTC > gameB.gameTimeUTC) {
+            return 1;
+        } else if (gameB.gameTimeUTC > gameA.gameTimeUTC) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+
+    // with the games sorted, place them in the correct collection
+    availableGames = [];
+    archivedGames = [];
+    futureGames = [];
+    for (i = 0; i < games.length; i++) {
+        game = games[i];
+        switch (game.gameState) {
+            case "available":
+                availableGames.push(game);
+                break;
+            case "archived":
+                archivedGames.push(game);
+                break;
+            case "future":
+                futureGames.push(game);
+                break;
+            default:
+                console.error("unable to place game: " + game);
+                break;
+        }
+    }
+
+    // archived games should be last one first, so
+    // that the user sees the latest archived.
+    archivedGames.reverse();
+
+    callback({
+        availableGames: availableGames,
+        archivedGames: archivedGames,
+        futureGames: futureGames
+    });
+}
+
+function updateWatchedGameForUser(userId, watchedGame, callback) {
+    // update the games watched with this watched game information
+    User.findById(userId, function(err, user) {
+        var i, gamesWatched, existingGameWatched, foundMatch;
+
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        // find the watched games for this watched game sport
+        for (i = 0; i < user.sportsWatched.length; i++) {
+            if (watchedGame.sport === user.sportsWatched[i].sport) {
+                gamesWatched = user.sportsWatched[i].games;
+                break;
+            }
+        }
+
+        // if no games watched yet for this sport, create one
+        if (!gamesWatched) {
+            gamesWatched = [];
+            user.sportsWatched.push(gamesWatched);
+        }
+
+        // loop through the games watched looking for a matching game
+        for (i = 0; i < gamesWatched.length; i++) {
+            if (watchedGame._id === gamesWatched[i].game.toJSON()) {
+                foundMatch = true;
+                break;
+            }
+        }
+
+        if (foundMatch) {
+            // if one already exists, update the existing game
+            gamesWatched[i].notes = watchedGame.notes;
+            gamesWatched[i].completed = watchedGame.completed;
+        } else {
+            // else create a new one
+            gamesWatched.push({
+                game: watchedGame._id,
+                notes: watchedGame.notes,
+                completed: watchedGame.completed
+            });
+        }
+
+        // save the updated game
+        user.save(function(err, user) {
+            callback(err);
+        });
+    });
+}
