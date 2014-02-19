b6dbb02a239b9e8e290b74faa1bf2fe231fbd556
Dylan Smith <dylantsmith@gmail.com>
2014-02-19 17:07:45 -0600

    Refactor team schedule update
    Pull the common logic for the team schedule update into a library, and
    call that within the schedule controllers.

--- controllers/nba-schedule.js
+++ controllers/nba-schedule.js
@@ -2,6 +2,7 @@ var csv = require("csv"),
     request = require("request"),
     async = require("async"),
     NBAGameBuilder = require("../lib/nba-game-builder"),
+    teamSchedule = require("../lib/team-schedule"),
     mongoose = require("mongoose"),
     Team = mongoose.model("Team");
 
@@ -36,136 +37,28 @@ module.exports = function(app) {
         });
 
         app.get("/teams/:team/update-schedule", function(req, res) {
-            var teamName, nbaGames, nbaGameBuilder;
+            var teamName, nbaGameBuilder;
 
             teamName = req.params.team;
-            nbaGames = [];
             nbaGameBuilder = new NBAGameBuilder();
 
-            async.waterfall([
-                // Get the schedule information
-                function(callback) {
-                    request.get(getScheduleUrl(teamName),
-                        function(error, response, body) {
-                            callback(null, body);
-                        });
-                },
-                // Get the NBA team we're trying to load the schedule for
-                function(csvString, callback) {
-                    Team.findOne({
-                        sport: "NBA",
-                        name: teamName
-                    }, function(err, team) {
-                        if (err) {
-                            callback(err);
-                        } else {
-                            callback(null, csvString, team);
-                        }
-                    });
-                },
-                // Parse the schedule into individual games
-                function(csvString, nbaTeam, callback) {
-                    var valuesForNBAGames = [];
+            teamSchedule.update(getScheduleUrl(teamName), "NBA", teamName, {
+                    date: 1,
+                    time: 2,
+                    teams: 0,
+                    location: 3,
+                    description: 4
+                }, nbaGameBuilder, app,
+                function(err) {
+                    if (err) {
+                        console.log(err);
+                        res.send(500, err);
+                        return;
+                    }
 
-                    csv().from.string(csvString, {
-                        delimiter: ",",
-                        escape: '"'
-                    })
-                    // when a record is found in the CSV file (a row)
-                    .on("record", function(row, index) {
-                        var nbaGame;
-
-                        // skip the header row
-                        if (index === 0) {
-                            return;
-                        }
-
-                        // store these values in an array to be processed later
-                        // date, time, teams, location, networks
-                        valuesForNBAGames.push({
-                            date: row[1].trim(),
-                            time: row[2].trim(),
-                            teams: row[0].trim(),
-                            location: row[3].trim(),
-                            description: row[4].trim()
-                        });
-                    })
-                    // when the end of the CSV document is reached
-                    .on("end", function() {
-                        // call the next function with the values for these games
-                        callback(null, valuesForNBAGames, nbaTeam);
-                    })
-                    // if any errors occur
-                    .on("error", function(error) {
-                        // call the callback with the error message
-                        callback(error.message);
-                    });
-                },
-                // Find or create each Game using the builder
-                function(valuesForNBAGames, nbaTeam, callback) {
-                    var count, nbaGames;
-
-                    nbaGames = [];
-                    count = 0;
-                    // We gotta create these NBA games asynchronously,
-                    // so use an inner async block
-                    async.whilst(
-                        function() {
-                            return count < valuesForNBAGames.length;
-                        },
-                        function(whilstCallback) {
-                            var nbaGameValues;
-
-                            nbaGameValues = valuesForNBAGames[count];
-                            count++;
-                            // date, time, teams, location, description
-                            nbaGameBuilder.buildNBAGame(
-                                app,
-                                nbaGameValues.date,
-                                nbaGameValues.time,
-                                nbaGameValues.teams,
-                                nbaGameValues.location,
-                                nbaGameValues.description,
-                                function(err, nbaGame) {
-                                    if (err) {
-                                        whilstCallback(err);
-                                    } else {
-                                        // add the game to our list
-                                        nbaGames.push(nbaGame);
-                                        whilstCallback();
-                                    }
-                                });
-                        },
-                        function(err) {
-                            if (err) {
-                                callback(err);
-                            } else {
-                                callback(null, nbaGames, nbaTeam);
-                            }
-                        }
-                    );
-                },
-                // Save the schedule to the team
-                function(nbaGames, nbaTeam, callback) {
-                    // store the games on the team
-                    nbaTeam.schedule = nbaGames;
-                    nbaTeam.save(function(err) {
-                        if (err) {
-                            callback(err);
-                        } else {
-                            callback(null);
-                        }
-                    });
+                    res.send("done!");
                 }
-            ], function(err, result) {
-                if (err) {
-                    console.log(err);
-                    res.send(500, err);
-                    return;
-                }
-
-                res.send("done!");
-            });
+            );
 
         });
     });
--- controllers/nhl-schedule.js
+++ controllers/nhl-schedule.js
@@ -2,6 +2,7 @@ var csv = require("csv"),
     request = require("request"),
     async = require("async"),
     NHLGameBuilder = require("../lib/nhl-game-builder"),
+    teamSchedule = require("../lib/team-schedule"),
     mongoose = require("mongoose"),
     Team = mongoose.model("Team");
 
@@ -36,137 +37,28 @@ module.exports = function(app) {
         });
 
         app.get("/teams/:team/update-schedule", function(req, res) {
-            var teamName, nhlGames, nhlGameBuilder;
+            var teamName, nhlGameBuilder;
 
             teamName = req.params.team;
-            nhlGames = [];
             nhlGameBuilder = new NHLGameBuilder();
 
-            async.waterfall([
-                // Get the schedule information
-                function(callback) {
-                    request.get(getScheduleUrl(teamName),
-                        function(error, response, body) {
-                            callback(null, body);
-                        });
-                },
-                // Get the NHL team we're trying to load the schedule for
-                function(csvString, callback) {
-                    Team.findOne({
-                        sport: "NHL",
-                        name: teamName
-                    }, function(err, team) {
-                        if (err) {
-                            callback(err);
-                        } else {
-                            callback(null, csvString, team);
-                        }
-                    });
-                },
-                // Parse the schedule into individual games
-                function(csvString, nhlTeam, callback) {
-                    var valuesForNHLGames = [];
+            teamSchedule.update(getScheduleUrl(teamName), "NHL", teamName, {
+                    date: 0,
+                    time: 1,
+                    teams: 3,
+                    location: 4,
+                    description: 5
+                }, nhlGameBuilder, app,
+                function(err) {
+                    if (err) {
+                        console.log(err);
+                        res.send(500, err);
+                        return;
+                    }
 
-                    csv().from.string(csvString, {
-                        delimiter: ",",
-                        escape: '"'
-                    })
-                    // when a record is found in the CSV file (a row)
-                    .on("record", function(row, index) {
-                        var nhlGame;
-
-                        // skip the header row
-                        if (index === 0) {
-                            return;
-                        }
-
-                        // store these values in an array to be processed later
-                        // date, time, teams, location, networks
-                        valuesForNHLGames.push({
-                            date: row[0].trim(),
-                            time: row[1].trim(),
-                            teams: row[3].trim(),
-                            location: row[4].trim(),
-                            networks: row[5].trim()
-                        });
-                    })
-                    // when the end of the CSV document is reached
-                    .on("end", function() {
-                        // call the next function with the values for these games
-                        callback(null, valuesForNHLGames, nhlTeam);
-                    })
-                    // if any errors occur
-                    .on("error", function(error) {
-                        // call the callback with the error message
-                        callback(error.message);
-                    });
-                },
-                // Find or create each Game using the builder
-                function(valuesForNHLGames, nhlTeam, callback) {
-                    var count, nhlGames;
-
-                    nhlGames = [];
-                    count = 0;
-                    // We gotta create these NHL games asynchronously,
-                    // so use an inner async block
-                    async.whilst(
-                        function() {
-                            return count < valuesForNHLGames.length;
-                        },
-                        function(whilstCallback) {
-                            var nhlGameValues;
-
-                            nhlGameValues = valuesForNHLGames[count];
-                            count++;
-                            // date, time, teams, location, networks
-                            nhlGameBuilder.buildNHLGame(
-                                app,
-                                nhlGameValues.date,
-                                nhlGameValues.time,
-                                nhlGameValues.teams,
-                                nhlGameValues.location,
-                                nhlGameValues.networks,
-                                function(err, nhlGame) {
-                                    if (err) {
-                                        whilstCallback(err);
-                                    } else {
-                                        // add the game to our list
-                                        nhlGames.push(nhlGame);
-                                        whilstCallback();
-                                    }
-                                });
-                        },
-                        function(err) {
-                            if (err) {
-                                callback(err);
-                            } else {
-                                callback(null, nhlGames, nhlTeam);
-                            }
-                        }
-                    );
-                },
-                // Save the schedule to the team
-                function(nhlGames, nhlTeam, callback) {
-                    // store the games on the team
-                    nhlTeam.schedule = nhlGames;
-                    nhlTeam.save(function(err) {
-                        if (err) {
-                            callback(err);
-                        } else {
-                            callback(null);
-                        }
-                    });
+                    res.send("done!");
                 }
-            ], function(err, result) {
-                if (err) {
-                    console.log(err);
-                    res.send(500, err);
-                    return;
-                }
-
-                res.send("done!");
-            });
-
+            );
         });
     });
 };
