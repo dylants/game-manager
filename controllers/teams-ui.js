547db5fd272126a875b19bd3c87ee8e1d4ef8162
Dylan Smith <dylantsmith@gmail.com>
2014-03-02 13:19:54 -0600

    Fix jshint problems
    Move function definition outside of for loop

--- controllers/teams-ui.js
+++ controllers/teams-ui.js
@@ -2,6 +2,27 @@ var _ = require("underscore"),
     mongoose = require("mongoose"),
     Team = mongoose.model("Team");
 
+
+function sortTeams(teams) {
+    teams.sort(function(teamA, teamB) {
+        // sort by city
+        if (teamA.city > teamB.city) {
+            return 1;
+        } else if (teamA.city < teamB.city) {
+            return -1;
+        } else {
+            // same city, sort by mascot
+            if (teamA.mascot > teamB.mascot) {
+                return 1;
+            } else if (teamA.mascot < teamB.mascot) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    });
+}
+
 module.exports = function(app) {
     app.namespace("/api", function() {
         app.get("/teams-ui", function(req, res) {
@@ -33,23 +54,7 @@ module.exports = function(app) {
                 // sort the teams
                 sportKeys = _.keys(returnTeams);
                 for (count = 0; count < sportKeys.length; count++) {
-                    returnTeams[sportKeys[count]].sort(function(teamA, teamB) {
-                    // sort by city
-                    if (teamA.city > teamB.city) {
-                        return 1;
-                    } else if (teamA.city < teamB.city) {
-                        return -1;
-                    } else {
-                        // same city, sort by mascot
-                        if (teamA.mascot > teamB.mascot) {
-                            return 1;
-                        } else if (teamA.mascot < teamB.mascot) {
-                            return -1;
-                        } else {
-                            return 0;
-                        }
-                    }
-                    });
+                    sortTeams(returnTeams[sportKeys[count]]);
                 }
 
                 res.send(returnTeams);
