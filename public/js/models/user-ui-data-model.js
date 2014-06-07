fbc0ba1cb7c73a264f9951ba85c09d0428da2f72
Dylan Smith <dylantsmith@gmail.com>
2014-06-07 15:47:21 -0500

    UserUIDataModel is never new
    Since we've removed the ID from this model, Backbone thinks its new.
    State that it's not to avoid the POST request and instead generate a
    PATCH request.

--- public/js/models/user-ui-data-model.js
+++ public/js/models/user-ui-data-model.js
@@ -4,6 +4,12 @@ define([
     "backbone"
 ], function(Backbone) {
     return Backbone.Model.extend({
-        url: "/api/user-ui-data/"
+        url: "/api/user-ui-data",
+
+        isNew: function() {
+            // the data in this model is never new,
+            // we're always performing a PATCH instead of POST
+            return false;
+        }
     });
 });
